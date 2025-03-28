514da05e7b39d244eb9de5da41d3b0f77afa6895
Leon Rodrigues <rodriguesleon001@gmail.com>
2025-03-28 19:44:25 -0300

    add automatic image slider for "Parceiros"

--- assents/JS/main.js
+++ assents/JS/main.js
@@ -124,3 +124,35 @@ document.querySelectorAll('input').forEach(($input) => {
     e.target.value = formato[field](e.target.value)
   }, false)
 })
+
+// Slides parceiros
+
+var index = 0;
+var slides = document.querySelectorAll(".slides");
+var dot = document.querySelectorAll(".dot");
+
+function changeSlide(){
+
+  if(index<0){
+    index = slides.length-1;
+  }
+  
+  if(index>slides.length-1){
+    index = 0;
+  }
+  
+  for(let i=0;i<slides.length;i++){
+    slides[i].style.display = "none";
+    dot[i].classList.remove("active");
+  }
+  
+  slides[index].style.display= "block";
+  dot[index].classList.add("active");
+  
+  index++;
+  
+  setTimeout(changeSlide,3000);
+  
+}
+
+changeSlide();
